e96e30ddaa37bf2781d7a8464c3121882cdfb608
jobayerrahman <jobayer.aiub@gmail.com>
2023-06-28 23:58:09 +0600

    Sass add and pages file create

--- app/layouts/Default/Default.js
+++ app/layouts/Default/Default.js
@@ -1,10 +1,10 @@
 import Header from "../../modules/Header/Header";
 import styles from './Default.module.css'
-export default function Default({Children}){
+export default function Default({children}){
         return(
             <div className={styles.default}>
                 <Header/>
-                <div>{Children}</div>
+                <div>{children}</div>
             </div>
         );
 }
--- app/templates/HomePage/HomePage.js
+++ app/templates/HomePage/HomePage.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import styles from './HomePage.module.css';
+import styles from './HomePage.module.scss';
+
 class HomePage extends Component {
     render(){
         return (
             <>
-                <h2>Home Page Content</h2>
+                <h2 className={styles.homeTitle}>Home Page Content</h2>
             </>
         )
     }
