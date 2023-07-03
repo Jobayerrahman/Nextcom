f374fa348caadffe2a4b8bb37a7dec6c47a119a5
jobayerrahman <jobayer.aiub@gmail.com>
2023-07-04 00:52:14 +0600

    Service card new style add

--- app/components/Services/Service.js
+++ app/components/Services/Service.js
@@ -1,15 +1,18 @@
 import { Component } from "react";
 import Image from "next/image";
 import styles from './Service.module.scss';
+import Button from '@mui/material/Button';
 
 class Service extends Component{
     render(){
         return(
             <div className={styles.serviceCard}>
-                <Image className={styles.servicesImages} src={this.props.img}/>
-                <h1 className={styles.serviceTitle}>Heading Title</h1>
-                <p className={styles.servicesDescribtion}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                <Image className={styles.serviceImage} src={this.props.img}/>
+                <div className={styles.serviceCardBody}>
+                    <h1 className={styles.serviceTitle}>Heading Title</h1>
+                    <p className={styles.servicesDescribtion}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <Button className={styles.serviceButton}>Read more</Button>
+                </div>
             </div>
         );
     }
