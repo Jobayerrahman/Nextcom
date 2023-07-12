7b35b567912a8af9666cce78ad7c537b9a96290b
jobayerrahman <jobayer.aiub@gmail.com>
2023-07-12 14:56:53 +0600

    Summary compnent create with react countup

---
+++ app/components/Summary/Summary.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Grid from '@mui/material/Grid';
+import styles from './Summary.module.scss';
+import CountUp from 'react-countup';
+import VisibilitySensor from "react-visibility-sensor";
+class Summary extends Component {
+    render() {
+        return (
+            <div className={styles.summaryWrapper}>
+                <Grid container spacing={4}>
+                    <Grid item xs={3}>
+                        <h1 className={styles.counterNumber}>
+                            <CountUp start={0} end={4220}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor onChange={start} delayedCall>
+                                <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}
+                            </CountUp>
+                        </h1>
+                        <h4 className={styles.counterTitle}>Working Hours</h4>
+                        <hr className={styles.summaryUnderbar}/>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <h1 className={styles.counterNumber}>
+                            <CountUp start={0} end={290}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor onChange={start} delayedCall>
+                                <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}
+                            </CountUp>
+                        </h1>
+                        <h4 className={styles.counterTitle}>Total Projects</h4>
+                        <hr className={styles.summaryUnderbar}/>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <h1 className={styles.counterNumber}>
+                            <CountUp start={0} end={150}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor onChange={start} delayedCall>
+                                <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}
+                            </CountUp>
+                        </h1>
+                        <h4 className={styles.counterTitle}>Work Completed</h4>
+                        <hr className={styles.summaryUnderbar}/>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <h1 className={styles.counterNumber}>
+                            <CountUp start={0} end={67}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor onChange={start} delayedCall>
+                                <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}
+                            </CountUp>
+                        </h1>
+                        <h4 className={styles.counterTitle}>Total Client</h4>
+                        <hr className={styles.summaryUnderbar}/>
+                    </Grid>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default Summary;
--- app/templates/HomePage/HomePage.js
+++ app/templates/HomePage/HomePage.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import styles from './HomePage.module.scss';
 import TopBanner from "../../components/Banner/TopBanner";
 import AllService from "../../components/Services/AllService";
+import Summary from "../../components/Summary/Summary";
 class HomePage extends Component {
     render(){
         return (
             <>
                 <TopBanner/>
                 <AllService/>
+                <Summary/>
             </>
         )
     }
